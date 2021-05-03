0bf8ed5f984b21bac42479c13dea2b971901b1be
Maurice Jones <jones.maurice39@yahoo.com>
2021-05-02 19:46:00 -0700

    favs router

--- nucampsiteServer/config.js
+++ nucampsiteServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '323757945825060',
+        clientSecret: 'ef5062c6dc3184e710ad8a8b762de6cf'
+    }
 }
